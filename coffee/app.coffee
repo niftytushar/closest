94adc566e993a934c482fe6e7adadcdc46deda53
Tushar Agarwal <niftytushar@gmail.com>
2014-08-25 23:45:37 +0530

    Added Google Map

--- coffee/app.coffee
+++ coffee/app.coffee
@@ -1,1 +1,19 @@
-message = "Hello Coffee!"
+mapOptions =
+	center: new google.maps.LatLng(-34.397, 150.644)
+	zoom: 8
+	disableDoubleClickZoom: true
+
+map = new google.maps.Map document.getElementById("map-canvas"), mapOptions
+
+_mapDblClick = () ->
+	alert "Dbl Clicked"
+
+addMarker = (options) ->
+	markerOptions =
+		position: options.LatLng
+		draggable: true
+		map: options.map || null
+
+	google.maps.Marker markerOptions
+
+google.maps.event.addListener map, "dblclick", _mapDblClick
--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
 (function() {
-  var message;
+  var addMarker, map, mapOptions, _mapDblClick;
 
-  message = "Hello Coffee!";
+  mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8,
+    disableDoubleClickZoom: true
+  };
+
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+  _mapDblClick = function() {
+    return alert("Dbl Clicked");
+  };
+
+  addMarker = function(options) {
+    var markerOptions;
+    markerOptions = {
+      position: options.LatLng,
+      draggable: true,
+      map: options.map || null
+    };
+    return google.maps.Marker(markerOptions);
+  };
+
+  google.maps.event.addListener(map, "dblclick", _mapDblClick);
 
 }).call(this);
