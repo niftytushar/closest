529b56e4932c2a5fa0533497f3bc7a0e8a894274
Tushar Agarwal <niftytushar@gmail.com>
2014-08-31 20:46:23 +0530

    updates add quick tip replicate drop offs implement double click marker add

--- coffee/app.coffee
+++ coffee/app.coffee
@@ -1,21 +1,54 @@
 mapOptions =
-	center: new google.maps.LatLng(-34.397, 150.644)
-	zoom: 8
+	center: new google.maps.LatLng(20.592616, 78.962860)
+	zoom: 4
 	disableDoubleClickZoom: true
 
+# The global map object
 map = new google.maps.Map document.getElementById("map-canvas"), mapOptions
 
+# Contains all markers - destination and all drop off points
+markers =
+	destination: null
+	dropOff: []
 
-markerOptions =
+createMarker = (options) ->
+	markerOptions =
 		draggable: true
 		map: map
+		position: options.latLng
 
-marker = new google.maps.Marker markerOptions
+	# Create a marker
+	marker = new google.maps.Marker markerOptions
 
-google.maps.event.addListener marker, "position_changed", () ->
-		alert "Moved"
+	google.maps.event.addListener marker, "dragend", () ->
+		console.log "Marker was moved."
 
+	# Return marker object for further use
+	marker
+
+# Map double click handler
 _mapDblClick = (ev) ->
-	marker.setPosition ev.latLng
+	# Check if a marker should added
+		# Check destination marker
+		# Check drop-off locations marker
+
+	# Add a marker if required
+	createMarker ev
 	
 google.maps.event.addListener map, "dblclick", _mapDblClick
+
+addDropOff = (ev) ->
+	ev && ev.preventDefault()
+
+	$(".__t_drop-offs").append """
+	<div class="input-group">
+        <input type="text" class="form-control" placeholder="Start typing your location..." />
+        <span class="input-group-btn">
+            <button class="btn btn-default" type="button" title="Use my current location"><i class="fa fa-map-marker"></i></button>
+        </span>
+    </div><!-- /input-group -->
+    """
+addDropOff()
+
+# Handle add more drop off locations
+$("#addDropOff").on "click", addDropOff
--- js/app.js
+++ js/app.js
@@ -1,29 +1,46 @@
 (function() {
-  var map, mapOptions, marker, markerOptions, _mapDblClick;
+  var addDropOff, createMarker, map, mapOptions, markers, _mapDblClick;
 
   mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 8,
+    center: new google.maps.LatLng(20.592616, 78.962860),
+    zoom: 4,
     disableDoubleClickZoom: true
   };
 
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-  markerOptions = {
-    draggable: true,
-    map: map
+  markers = {
+    destination: null,
+    dropOff: []
   };
 
-  marker = new google.maps.Marker(markerOptions);
-
-  google.maps.event.addListener(marker, "position_changed", function() {
-    return alert("Moved");
-  });
+  createMarker = function(options) {
+    var marker, markerOptions;
+    markerOptions = {
+      draggable: true,
+      map: map,
+      position: options.latLng
+    };
+    marker = new google.maps.Marker(markerOptions);
+    google.maps.event.addListener(marker, "dragend", function() {
+      return console.log("Marker was moved.");
+    });
+    return marker;
+  };
 
   _mapDblClick = function(ev) {
-    return marker.setPosition(ev.latLng);
+    return createMarker(ev);
   };
 
   google.maps.event.addListener(map, "dblclick", _mapDblClick);
+
+  addDropOff = function(ev) {
+    ev && ev.preventDefault();
+    return $(".__t_drop-offs").append("<div class=\"input-group\">\n        <input type=\"text\" class=\"form-control\" placeholder=\"Start typing your location...\" />\n        <span class=\"input-group-btn\">\n            <button class=\"btn btn-default\" type=\"button\" title=\"Use my current location\"><i class=\"fa fa-map-marker\"></i></button>\n        </span>\n    </div><!-- /input-group -->");
+  };
+
+  addDropOff();
+
+  $("#addDropOff").on("click", addDropOff);
 
 }).call(this);
