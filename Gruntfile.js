72b009e1388315cea7708e20ad69fa3399c8c42c
Tushar Agarwal <niftytushar@gmail.com>
2014-08-31 21:04:36 +0530

    Added watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,21 @@ module.exports = function(grunt) {
             }
         },
         compass: {
-          compile: {
-            options: {
-                config: "config.rb"
+            compile: {
+                options: {
+                    config: "config.rb"
+                }
             }
-          }
+        },
+        watch: {
+            css: {
+                files: ['sass/*.scss'],
+                tasks: ['compass']
+            },
+            scripts: {
+                files: ['coffee/app.coffee'],
+                tasks: ['coffee']
+            }
         }
     });
 
@@ -24,6 +34,9 @@ module.exports = function(grunt) {
 
     // Load the plugin that provides the "compass" task
     grunt.loadNpmTasks('grunt-contrib-compass');
+
+    // Load the plugin that provides the "watch" task
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
     grunt.registerTask('default', ['coffee', 'compass']);
