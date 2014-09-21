467a5591f63f58a3f3339830ea726f48523b41fe
Tushar Agarwal <niftytushar@gmail.com>
2014-09-21 20:08:54 +0530

    add method to remove markers print text result display route calculate closest location add Google Direction Matrix add Google Direction Renderer update .gitignore

--- coffee/app.coffee
+++ coffee/app.coffee
@@ -1,3 +1,6 @@
+_ajaxError = (jqXHR, textStatus, errorThrown) ->
+	console.log "AJAX Error: #{errorThrown}"
+
 mapOptions =
 	center: new google.maps.LatLng(20.592616, 78.962860)
 	zoom: 4
@@ -26,32 +29,46 @@ createMarker = (options) ->
 	# Return marker object for further use
 	marker
 
+removeAllMarkers = () ->
+	# Remove destination marker
+	markers.destination.setMap null
+
+	# Remove drop off location markers
+	for marker in markers.dropOff
+		marker.setMap null
+
 # Map double click handler
 _onMapDblClick = (ev) ->
 	# Check if a marker should added
-		# Check destination marker
-		# Check drop-off locations marker
+	
+	# Check destination marker
+	ev.isDestination = true if not markers.destination
+
+	# Check drop-off locations marker
 
 	# Attempt to create a marker
 	marker = createMarker ev
 
 	# Save this marker for future use
+	if ev.isDestination then markers.destination = marker else markers.dropOff.push marker
 	
 google.maps.event.addListener map, "dblclick", _onMapDblClick
 
 # Attach Google Places Autocomplete to destination
 autocomplete = new google.maps.places.Autocomplete document.getElementById("destination")
+# Type 1 autocomplete indicates destination
+autocomplete._type = 1
 
 # Handle place change
 _onPlaceChanged = () ->
-	place = autocomplete.getPlace()
+	place = this.getPlace()
 
 	if place.geometry
 		map.panTo place.geometry.location
 		map.setZoom 15
 
 		# Do some search / marker adding here
-		_onMapDblClick { 'latLng': place.geometry.location }
+		_onMapDblClick { 'latLng': place.geometry.location, 'isDestination': if (this._type is 1) then true else false }
 	else
 		# Wanna do something else here? Go ahead.
 
@@ -64,16 +81,90 @@ google.maps.event.addListener autocomplete, 'place_changed', _onPlaceChanged
 addDropOff = (ev) ->
 	ev && ev.preventDefault()
 
-	$(".__t_drop-offs").append """
+	$dropOff = $(".__t_drop-offs").append """
 		<div class="input-group">
 	        <input type="text" class="form-control" placeholder="Start typing your location..." />
 	        <span class="input-group-btn">
-	            <button class="btn btn-default" type="button" title="Use my current location"><i class="fa fa-map-marker"></i></button>
+	            <button class="btn btn-default btn-map-trigger" type="button" title="Use my current location"><i class="fa fa-map-marker"></i></button>
 	        </span>
 	    </div><!-- /input-group -->
 	    """
+	# Attach Google Places Autocomplete to drop off
+	_autocomplete = new google.maps.places.Autocomplete $dropOff.find("input[type='text']:last")[0]
+	# Type 2 autocomplete indicates drop off location
+	_autocomplete._type = 2
+
+	# Register place changed / selected handler
+	google.maps.event.addListener _autocomplete, 'place_changed', _onPlaceChanged
+
+	""
+
 # Add first drop off location box
 addDropOff()
 
 # Handle add more drop off locations
 $("#addDropOff").on "click", addDropOff
+
+_displayRoute = (result, status) ->
+	directionsRendererOptns =
+		'directions': result
+		'hideRouteList': true
+		'map': map
+
+	directionsRenderer = new google.maps.DirectionsRenderer directionsRendererOptns
+
+
+_distanceCalculated = (response, status) ->
+	# TODO: Handle status and display appropriate message
+
+	rows = response.rows
+	closest =
+		'distance':
+			'value': Infinity
+
+	for row, i in rows
+		row.elements[0].index = i
+		if row.elements[0].distance.value < closest.distance.value then closest = row.elements[0]
+
+	closest.location =
+		text: $(".__t_drop-offs").find("input[type='text']:eq(#{closest.index})").val()
+
+	# Display result text
+	$("#result").html "Your closest drop off is <strong>#{closest.location.text}</strong>, #{closest.distance.text} away."
+
+	# Map cleanup
+	removeAllMarkers()
+
+	# Display directions to location
+	directionsService = new google.maps.DirectionsService()
+
+	directionsRequestOptns =
+		'destination': markers.destination.getPosition()
+		'origin': markers.dropOff[closest.index].getPosition()
+		'travelMode': google.maps.TravelMode.DRIVING
+
+	directionsService.route directionsRequestOptns, _displayRoute
+
+findClosest = (ev) ->
+	ev.preventDefault()
+
+	distancematrix = new google.maps.DistanceMatrixService()
+
+	# Get latitude, longitude of destination
+	destination = markers.destination.getPosition()
+
+	# Get latitude, longitude of all drop off locations
+	origins = []
+	for origin in markers.dropOff
+		origins.push origin.getPosition().lat() + "," + origin.getPosition().lng()
+
+	requestParams =
+		'travelMode': google.maps.TravelMode.DRIVING
+		'origins': origins
+		'destinations': [destination]
+
+	# Get distance to destination from all drop off locations
+	distancematrix.getDistanceMatrix requestParams, _distanceCalculated
+
+# Handle click on Find Closest button
+$("#findClosestForm").on "submit", findClosest
--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 (function() {
-  var addDropOff, autocomplete, createMarker, map, mapOptions, markers, _onMapDblClick, _onPlaceChanged;
+  var addDropOff, autocomplete, createMarker, findClosest, map, mapOptions, markers, removeAllMarkers, _ajaxError, _displayRoute, _distanceCalculated, _onMapDblClick, _onPlaceChanged;
+
+  _ajaxError = function(jqXHR, textStatus, errorThrown) {
+    return console.log("AJAX Error: " + errorThrown);
+  };
 
   mapOptions = {
     center: new google.maps.LatLng(20.592616, 78.962860),
@@ -28,23 +32,46 @@
     return marker;
   };
 
+  removeAllMarkers = function() {
+    var marker, _i, _len, _ref, _results;
+    markers.destination.setMap(null);
+    _ref = markers.dropOff;
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      marker = _ref[_i];
+      _results.push(marker.setMap(null));
+    }
+    return _results;
+  };
+
   _onMapDblClick = function(ev) {
     var marker;
-    return marker = createMarker(ev);
+    if (!markers.destination) {
+      ev.isDestination = true;
+    }
+    marker = createMarker(ev);
+    if (ev.isDestination) {
+      return markers.destination = marker;
+    } else {
+      return markers.dropOff.push(marker);
+    }
   };
 
   google.maps.event.addListener(map, "dblclick", _onMapDblClick);
 
   autocomplete = new google.maps.places.Autocomplete(document.getElementById("destination"));
 
+  autocomplete._type = 1;
+
   _onPlaceChanged = function() {
     var place;
-    place = autocomplete.getPlace();
+    place = this.getPlace();
     if (place.geometry) {
       map.panTo(place.geometry.location);
       map.setZoom(15);
       _onMapDblClick({
-        'latLng': place.geometry.location
+        'latLng': place.geometry.location,
+        'isDestination': this._type === 1 ? true : false
       });
     } else {
 
@@ -55,12 +82,77 @@
   google.maps.event.addListener(autocomplete, 'place_changed', _onPlaceChanged);
 
   addDropOff = function(ev) {
+    var $dropOff, _autocomplete;
     ev && ev.preventDefault();
-    return $(".__t_drop-offs").append("<div class=\"input-group\">\n	        <input type=\"text\" class=\"form-control\" placeholder=\"Start typing your location...\" />\n	        <span class=\"input-group-btn\">\n	            <button class=\"btn btn-default\" type=\"button\" title=\"Use my current location\"><i class=\"fa fa-map-marker\"></i></button>\n	        </span>\n	    </div><!-- /input-group -->");
+    $dropOff = $(".__t_drop-offs").append("<div class=\"input-group\">\n	        <input type=\"text\" class=\"form-control\" placeholder=\"Start typing your location...\" />\n	        <span class=\"input-group-btn\">\n	            <button class=\"btn btn-default btn-map-trigger\" type=\"button\" title=\"Use my current location\"><i class=\"fa fa-map-marker\"></i></button>\n	        </span>\n	    </div><!-- /input-group -->");
+    _autocomplete = new google.maps.places.Autocomplete($dropOff.find("input[type='text']:last")[0]);
+    _autocomplete._type = 2;
+    google.maps.event.addListener(_autocomplete, 'place_changed', _onPlaceChanged);
+    return "";
   };
 
   addDropOff();
 
   $("#addDropOff").on("click", addDropOff);
+
+  _displayRoute = function(result, status) {
+    var directionsRenderer, directionsRendererOptns;
+    directionsRendererOptns = {
+      'directions': result,
+      'hideRouteList': true,
+      'map': map
+    };
+    return directionsRenderer = new google.maps.DirectionsRenderer(directionsRendererOptns);
+  };
+
+  _distanceCalculated = function(response, status) {
+    var closest, directionsRequestOptns, directionsService, i, row, rows, _i, _len;
+    rows = response.rows;
+    closest = {
+      'distance': {
+        'value': Infinity
+      }
+    };
+    for (i = _i = 0, _len = rows.length; _i < _len; i = ++_i) {
+      row = rows[i];
+      row.elements[0].index = i;
+      if (row.elements[0].distance.value < closest.distance.value) {
+        closest = row.elements[0];
+      }
+    }
+    closest.location = {
+      text: $(".__t_drop-offs").find("input[type='text']:eq(" + closest.index + ")").val()
+    };
+    $("#result").html("Your closest drop off is <strong>" + closest.location.text + "</strong>, " + closest.distance.text + " away.");
+    removeAllMarkers();
+    directionsService = new google.maps.DirectionsService();
+    directionsRequestOptns = {
+      'destination': markers.destination.getPosition(),
+      'origin': markers.dropOff[closest.index].getPosition(),
+      'travelMode': google.maps.TravelMode.DRIVING
+    };
+    return directionsService.route(directionsRequestOptns, _displayRoute);
+  };
+
+  findClosest = function(ev) {
+    var destination, distancematrix, origin, origins, requestParams, _i, _len, _ref;
+    ev.preventDefault();
+    distancematrix = new google.maps.DistanceMatrixService();
+    destination = markers.destination.getPosition();
+    origins = [];
+    _ref = markers.dropOff;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      origin = _ref[_i];
+      origins.push(origin.getPosition().lat() + "," + origin.getPosition().lng());
+    }
+    requestParams = {
+      'travelMode': google.maps.TravelMode.DRIVING,
+      'origins': origins,
+      'destinations': [destination]
+    };
+    return distancematrix.getDistanceMatrix(requestParams, _distanceCalculated);
+  };
+
+  $("#findClosestForm").on("submit", findClosest);
 
 }).call(this);
