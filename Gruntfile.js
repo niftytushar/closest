d0aca96d09d2bc834ca94f1bd3ff188dd0c08666
sudeep <sudipmelepunathilmp@gmail.com>
2014-09-28 19:57:33 +0530

    icons added grunt webfont task added

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,14 +9,14 @@ module.exports = function(grunt) {
                     'js/app.js': 'coffee/app.coffee'
                 }
             }
-        },
+        },//coffee
         compass: {
             compile: {
                 options: {
                     config: "config.rb"
                 }
             }
-        },
+        },//compass
         watch: {
             css: {
                 files: ['sass/*.scss'],
@@ -26,7 +26,17 @@ module.exports = function(grunt) {
                 files: ['coffee/app.coffee'],
                 tasks: ['coffee']
             }
-        }
+        },//watch
+        webfont: {
+            icons: {
+                src: 'dev/icons/*.svg',
+                dest: 'build/fonts',
+                options: {
+                    stylesheet: 'scss',
+                    relativeFontPath: 'sass/icon-fonts/icons'
+                }
+            }
+        }//webfont
     });
 
     // Load the plugin that provides the "coffee" task.
@@ -37,6 +47,10 @@ module.exports = function(grunt) {
 
     // Load the plugin that provides the "watch" task
     grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Load the plugin that provides the "webfont" task
+    grunt.loadNpmTasks('grunt-webfont');
+
 
     // Default task(s).
     grunt.registerTask('default', ['coffee', 'compass']);
